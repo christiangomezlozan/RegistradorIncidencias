ece06025bac7fdd11257acf34b4b67957be227fd
christiangomezlozan <christian.gomloz@educa.jcyl.es>
2025-01-26 23:12:33 +0100

    Delete app directory

--- app/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    $conexion = new mysqli("localhost", "root", "", "registro_incidencias");
-
-    if($conexion -> connect_error){
-        die("Error de conexion" . $conexion->connect_error);
-    }
-
-?>
